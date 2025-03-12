17f63bbea84e86a9010c03d0fbf9ce47863d3722
bisht-ruchir-007 <ruchircr7@gmail.com>
2025-03-12 20:29:20 +0530

    first commit

--- src/main/java/com/app/practice/entity/VideoMetaData.java
+++ src/main/java/com/app/practice/entity/VideoMetaData.java
@@ -1,4 +1,4 @@
 package com.app.practice.entity;
 
-public class Video {
+public class VideoMetaData {
 }
---
+++ src/main/java/com/app/practice/repository/VideoMetaDataRepository.java
@@ -0,0 +1,4 @@
+package com.app.practice.repository;
+
+public interface VideoMetaDataRepository {
+}
