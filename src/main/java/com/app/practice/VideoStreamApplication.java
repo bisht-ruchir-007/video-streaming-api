7c7a40d2463428caa23f2abd26740c2f21bebd2e
bisht-ruchir-007 <ruchircr7@gmail.com>
2025-03-13 19:26:35 +0530

    lombok added in Entity, Config

--- src/main/java/com/app/practice/VideoStreamApplication.java
+++ src/main/java/com/app/practice/VideoStreamApplication.java
@@ -3,7 +3,6 @@ package com.app.practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 /*
  * Author : Ruchir Bisht
  * */
--- src/main/java/com/app/practice/entity/EngagementStatistics.java
+++ src/main/java/com/app/practice/entity/EngagementStatistics.java
@@ -1,19 +1,24 @@
 package com.app.practice.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Author: Ruchir Bisht
  * Entity class representing the 'engagement_statistics' table in the database.
  * This entity tracks video engagement metrics such as impressions and views.
  * It is associated with a single Video entity using a one-to-one relationship.* <p>
- * Note: Lombok is not working on the local machine; hence, getter, setter, and constructors are manually created.
  */
 @Entity
 @Table(
         name = "engagement_statistics",
         indexes = @Index(name = "idx_video_id", columnList = "video_id")
 )
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EngagementStatistics {
 
     // Primary key of the EngagementStatistics table
@@ -33,49 +38,49 @@ public class EngagementStatistics {
     @JoinColumn(name = "video_id", referencedColumnName = "videoId", nullable = false, unique = true)
     private Video video;
 
-    // Default constructor
-    public EngagementStatistics() {
-    }
+//    // Default constructor
+//    public EngagementStatistics() {
+//    }
 
-    // Constructor to initialize EngagementStatistics with specific values
-    public EngagementStatistics(Long id, Long impressions, Long views, Video video) {
-        this.id = id;
-        this.impressions = impressions;
-        this.views = views;
-        this.video = video;
-    }
-
-    // Getter and Setter methods
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getImpressions() {
-        return impressions;
-    }
-
-    public void setImpressions(Long impressions) {
-        this.impressions = impressions;
-    }
-
-    public Long getViews() {
-        return views;
-    }
-
-    public void setViews(Long views) {
-        this.views = views;
-    }
-
-    public Video getVideo() {
-        return video;
-    }
-
-    public void setVideo(Video video) {
-        this.video = video;
-    }
+//    // Constructor to initialize EngagementStatistics with specific values
+//    public EngagementStatistics(Long id, Long impressions, Long views, Video video) {
+//        this.id = id;
+//        this.impressions = impressions;
+//        this.views = views;
+//        this.video = video;
+//    }
+//
+//    // Getter and Setter methods
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Long getImpressions() {
+//        return impressions;
+//    }
+//
+//    public void setImpressions(Long impressions) {
+//        this.impressions = impressions;
+//    }
+//
+//    public Long getViews() {
+//        return views;
+//    }
+//
+//    public void setViews(Long views) {
+//        this.views = views;
+//    }
+//
+//    public Video getVideo() {
+//        return video;
+//    }
+//
+//    public void setVideo(Video video) {
+//        this.video = video;
+//    }
 }
